3b52802839767ac3c7788581960ba62b944f59b6
Deepak Gupta <Deepak4a@outlook.com>
2025-03-15 13:26:28 +0000

    code updated

---
+++ ResourceGroup/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "RG1"                     # Resource group name
+    storage_account_name  = "datalakestorage012"      # Storage account name
+    container_name        = "tfstate"                # Blob container name
+  }
+}
